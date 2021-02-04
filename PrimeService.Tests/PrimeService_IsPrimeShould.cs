3a28b58bc83a2076d4ba94ff82432abd9e9e3d14
Norm Moulton <normmoulton@yahoo.com>
2021-02-03 19:03:26 -0500

    Updates

--- PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -4,78 +4,85 @@ using System.Diagnostics;
 
 namespace Prime.UnitTests.Services
 {
-    [TestClass]
-    public class PrimeService_IsPrimeShould
-    {
-        // Conventional single value test methods.
-        [TestMethod]
-        public void IsPrime_1()
-        {
-            var primeService = new PrimeService();
-            bool result = primeService.IsPrime(1);
+   [TestClass]
+   public class PrimeService_IsPrimeShould
+   {
+      // Conventional single value test methods.
+      [TestMethod]
+      public void IsPrime_1()
+      {
+         int n = 1;
+         var primeService = new PrimeService();
+         bool result = primeService.IsPrime(n);
 
-            Assert.IsFalse(result, "1 should NOT be prime");
-        }
+         // This output will go into the "additional output" of the test.
+         Debug.WriteLine($"Testing {n}");
 
-        [TestMethod]
-        public void IsPrime_Neg()
-        {
-            var primeService = new PrimeService();
-            bool result = primeService.IsPrime(-10);
+         Assert.IsFalse(result, $"{n} should NOT be prime");
+      }
 
-            Assert.IsFalse(result, "Negative should NOT be prime");
-        }
+      [TestMethod]
+      public void IsPrime_Neg()
+      {
+         int n = -10;
+         var primeService = new PrimeService();
+         bool result = primeService.IsPrime(n);
 
-        // Unconventional multiple value test method.
-        [TestMethod]
-        public void IsPrime_0ToN()
-        {
-            // Call helper method.
-            IsPrime_N(0, false);
-            IsPrime_N(1, false);
-            IsPrime_N(2, true);
-            IsPrime_N(3, true);
-            IsPrime_N(4, false);
-            IsPrime_N(5, true);
-            IsPrime_N(6, false);
-            IsPrime_N(7, true);
-            IsPrime_N(8, false);
-            IsPrime_N(9, false);
-            IsPrime_N(10, false);
-            IsPrime_N(11, true);
-            IsPrime_N(12, false);
-            IsPrime_N(13, true);
-            IsPrime_N(14, false);
-            IsPrime_N(15, false);
-            IsPrime_N(16, false);
-            IsPrime_N(17, true);
-            IsPrime_N(18, false);
-            IsPrime_N(19, true);
-            IsPrime_N(20, false);
-            IsPrime_N(21, false);
-            IsPrime_N(22, false);
-            IsPrime_N(23, true);
-        }
+         // This output will go into the "additional output" of the test.
+         Debug.WriteLine($"Testing {n}");
 
-        // Helper method.
-        public void IsPrime_N(int n, bool isPrime)
-        {
-            var primeService = new PrimeService();
-            bool result = primeService.IsPrime(n);
+         Assert.IsFalse(result, "Negative should NOT be prime");
+      }
 
-            // Show what we did successfully before failure.
-            // If all the tests pass, this does not cause any output!
-            Debug.WriteLine($"Testing {n}");
+      // Unconventional multiple value test method.
+      [TestMethod]
+      public void IsPrime_0ToN()
+      {
+         // Call helper method.
+         IsPrime_N(0, false);
+         IsPrime_N(1, false);
+         IsPrime_N(2, true);
+         IsPrime_N(3, true);
+         IsPrime_N(4, false);
+         IsPrime_N(5, true);
+         IsPrime_N(6, false);
+         IsPrime_N(7, true);
+         IsPrime_N(8, false);
+         IsPrime_N(9, false);
+         IsPrime_N(10, false);
+         IsPrime_N(11, true);
+         IsPrime_N(12, false);
+         IsPrime_N(13, true);
+         IsPrime_N(14, false);
+         IsPrime_N(15, false);
+         IsPrime_N(16, false);
+         IsPrime_N(17, true);
+         IsPrime_N(18, false);
+         IsPrime_N(19, true);
+         IsPrime_N(20, false);
+         IsPrime_N(21, false);
+         IsPrime_N(22, false);
+         IsPrime_N(23, true);
+      }
 
-            // Decide the truth of the matter.
-            if (isPrime)
-            {
-                Assert.IsTrue(result, $"{n} should be prime");
-            }
-            else
-            {
-                Assert.IsFalse(result, $"{n} should be prime");
-            }
-        }
-    }
+      // Helper method.
+      public void IsPrime_N(int n, bool isPrime)
+      {
+         var primeService = new PrimeService();
+         bool result = primeService.IsPrime(n);
+
+         // This output will go into the "additional output" of the test.
+         Debug.WriteLine($"Testing {n}");
+
+         // Decide the truth of the matter.
+         if (isPrime)
+         {
+            Assert.IsTrue(result, $"{n} should be prime");
+         }
+         else
+         {
+            Assert.IsFalse(result, $"{n} should be prime");
+         }
+      }
+   }
 }
--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -1,31 +1,31 @@
-﻿using System;
+using System;
 
 namespace Prime.Services
 {
-    public class PrimeService
-    {
-        public bool IsPrime(int num)
-        {
-            // Assume true until proven false.
-            bool isPrime = true;
-            
-            if (num <= 1)
-            {
-                isPrime = false;
-            }
-            else
-            {
-                for (int div = num / 2; div > 1; div -= 1)
-                {
-                    if (num % div == 0)
-                    {
-                        isPrime = false;
-                        break;
-                    }
-                }
-            }
+   public class PrimeService
+   {
+      public bool IsPrime(int num)
+      {
+         // Assume true until proven false.
+         bool isPrime = true;
 
-            return isPrime;
-        }
-    }
+         if (num <= 1)
+         {
+            isPrime = false;
+         }
+         else
+         {
+            for (int div = num / 2; div > 1; div -= 1)
+            {
+               if (num % div == 0)
+               {
+                  isPrime = false;
+                  break;
+               }
+            }
+         }
+
+         return isPrime;
+      }
+   }
 }

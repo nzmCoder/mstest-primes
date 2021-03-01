5ba286e0afdfb7a461786af159410bc27b1aa2a2
NormMoulton <normmoulton@yahoo.com>
2021-02-28 20:30:36 -0500

    Refinements

--- clean.ps1
+++ clean.ps1
@@ -1,5 +1,8 @@
-dotnet clean
-Get-ChildItem * -Include obj -Recurse | Remove-Item -Recurse
-Get-ChildItem * -Include bin -Recurse | Remove-Item -Recurse
-Get-ChildItem * -Include TestResults -Recurse | Remove-Item -Recurse
-Remove-Item TestResults -Recurse 
+# dotnet clean
+Get-ChildItem * -Include obj -Recurse | Remove-Item -Recurse -ErrorAction Ignore
+Get-ChildItem * -Include bin -Recurse | Remove-Item -Recurse -ErrorAction Ignore
+Remove-Item PrimeService.Tests\TestResults -Recurse -ErrorAction Ignore
+Remove-Item ref -Recurse -ErrorAction Ignore
+Remove-Item *.exe -ErrorAction Ignore
+Remove-Item *.dll -ErrorAction Ignore
+Remove-Item *.json -ErrorAction Ignore
